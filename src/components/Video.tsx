a853825cd7745fb450de1ea91dc658d13c7d8f6b
evaldo <evaaldormj@gmail.com>
2025-04-04 11:23:54 -0300

    Feat: Create the Not Found page

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -2,7 +2,6 @@ import { DefaultUi, Player, Youtube } from "@vime/react";
 import '@vime/core/themes/default.css';
 import { Link } from "react-router-dom";
 import { Globe, Lightning } from "phosphor-react";
-// import { Link } from "react-router-dom";
 
 interface VideoProps {
     videoId: string;
@@ -26,11 +25,11 @@ export function Video(props: VideoProps) {
                         <p className="text-gray-700 mt-3 text-lg">Nessa aula vamos dar início ao entendimento da estrutura base do treinamento que será feito</p>
                     </div>
                     <div className="flex flex-col gap-2">
-                        <Link to={`/`} className="bg-blue-700 text-white text-center px-2 py-3 rounded flex justify-center items-center gap-1">
+                        <Link to={`/notFound`} className="bg-blue-700 text-white text-center px-2 py-3 rounded flex justify-center items-center gap-1">
                             <Globe size={18} />
                             Intranet
                         </Link>
-                        <Link to={`/`} className="bg-gray-700 text-white text-center px-2 py-3 rounded flex justify-center items-center gap-1">
+                        <Link to={`/notFound`} className="bg-gray-700 text-white text-center px-2 py-3 rounded flex justify-center items-center gap-1">
                             <Lightning size={18} />
                             Acesse o desafio
                         </Link>
